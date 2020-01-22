aba3901410bc70b8d7a704c3a20eb1007211fd26
nico.roesch <roesch@mediatis.de>
2020-01-22 14:41:01 +0100

    Added webpack bundling and moved setup method into its own file.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
-import { Schema, DOMParser } from "prosemirror-model"
-import { EditorView } from "prosemirror-view"
-import { EditorState } from "prosemirror-state"
-import { schema } from "prosemirror-schema-basic"
-import { addListNodes } from "prosemirror-schema-list"
-import { exampleSetup } from "prosemirror-example-setup"
-
-import { placeholderMark, placeholderPlugin } from "./placeholder"
-
-const demoSchema = new Schema({
-  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-  marks: schema.spec.marks.addToStart('placeholder', placeholderMark)
-})
-
-let state = EditorState.create({
-  doc: DOMParser.fromSchema(demoSchema).parse(document.querySelector("#content")),
-  plugins: exampleSetup({ schema: demoSchema }).concat(placeholderPlugin)
-})
-
+import { setupEditor } from "./setup"
 
 let elements = document.querySelectorAll(".full");
-for (let i = 0; i < elements.length; i++) {
-  new EditorView(elements[i], { state });
-}
+
+setupEditor(elements);
